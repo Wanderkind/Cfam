070f5d33f66e5ce2587c6c9b9b7f56590a00c5ec
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-06-27 19:04:31 +0900

    Add files via upload

---
+++ 02-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/*
+int main(void)
+{
+	printf("홍길동\n홍 길 동\n홍  길  동");
+    return 0;
+}
+*/
+
+
+int main(void)
+{
+	printf("이정섭\n");
+	printf("서울시 영등포구 여의도동\n");
+	printf("010-5927-7237");
+    return 0;
+}
---
+++ 02-2.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/*
+int main(void)
+{
+	printf("제 이름은 홍길동입니다.\n");
+	printf("제 나이는 %d살이고요.\n", 20);
+	printf("제가 사는 곳의 번지수는 %d-%d입니다.", 123, 456);
+	return 0;
+}
+*/
+
+int main(void)
+{
+	int a = 4;
+	int b = 5;
+	int c = 7;
+	int d = 9;
+	printf("%d×%d=%d\n", a, b, a*b);
+	printf("%d×%d=%d", c, d, c*d);
+	return 0;
+}
