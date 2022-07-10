afdb4bbc0fd0f0ac1d75a4d94d5e1c10b925229f
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-07-11 01:10:08 +0900

    Add files via upload

---
+++ 05-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+/*
+int main(void)
+{
+	int x,y;
+	scanf("%d %d",&x,&y);
+	int X,Y;
+	scanf("%d %d",&X,&Y);
+	printf("%d\n\n",(X-x)*(Y-y));
+	
+	return 0;
+}
+*/
+/*
+int main(void)
+{
+	double a,b;
+	scanf("%lf %lf", &a,&b);
+	printf("%f\n",a+b);
+	printf("%f\n",a-b);
+	printf("%f\n",a*b);
+	printf("%f\n\n",a/b);
+	
+	return 0;
+}
+*/
+/*
+int main(void)
+{	
+	int c;
+	scanf("%d",&c);
+	printf("%c\n\n",c);
+	
+	return 0;
+}
+*/
+/*
+int main(void)
+{
+	char d;
+	scanf("%c",&d);
+	printf("%d",d);
+	
+	return 0;
+}
+*/
